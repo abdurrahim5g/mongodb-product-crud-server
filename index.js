204f2188ae8dc400635142eaa49b624dff3f7091
f-rahim <freelancerrahim26@gmail.com>
2023-06-07 23:01:51 +0600

    Insert product data into MongoDB database from this server

--- index.js
+++ index.js
@@ -2,10 +2,30 @@ const express = require("express");
 const app = express();
 const port = process.env.PORT || 5000;
 const cors = require("cors");
+const MongoClient = require("mongodb").MongoClient;
+const uri =
+  "mongodb+srv://product-crud:RILvKKQbubqk8BH9@cluster0.undypbz.mongodb.net/?retryWrites=true&w=majority";
+const client = new MongoClient(uri);
 
 // Middleware
 app.use(cors());
 app.use(express.json());
+
+const run = async () => {
+  try {
+    const productCollection = client.db("productCRUD").collection("products");
+
+    // send data to the MongoDB server
+    app.post("/products", async (req, res) => {
+      const product = req.body;
+      const result = await productCollection.insertOne(product);
+      res.send(result);
+      console.log(result);
+    });
+  } finally {
+  }
+};
+run().catch(console.dir);
 
 // base API
 app.get("/", (req, res) => {
