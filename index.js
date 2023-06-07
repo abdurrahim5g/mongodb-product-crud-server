da8e9a0cee348911179135afd0f93bcddd957440
f-rahim <freelancerrahim26@gmail.com>
2023-06-07 23:50:38 +0600

    Setup single product API

--- index.js
+++ index.js
@@ -31,6 +31,14 @@ const run = async () => {
       res.send(result);
     });
 
+    // get Single product
+    app.get("/products/:id", async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const cursor = await productCollection.findOne(query);
+      res.send(cursor);
+    });
+
     // Delete operation
     app.delete("/products/:id", async (req, res) => {
       const id = req.params.id;
